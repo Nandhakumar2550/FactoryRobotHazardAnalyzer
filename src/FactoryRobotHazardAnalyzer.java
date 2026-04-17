bb1a5c42e6000a2d9fcfad1a434ed4ab1b56e859
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-04-18 00:55:24 +0530

    [Nandhakumar]\UC4-useConditions

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -8,7 +8,6 @@ public class FactoryRobotHazardAnalyzer {
 
         Scanner sc = new Scanner(System.in);
 
-        // UC2 → input
         System.out.print("Enter Arm Precision (0.0 - 1.0): ");
         double armPrecision = sc.nextDouble();
 
@@ -20,20 +19,35 @@ public class FactoryRobotHazardAnalyzer {
         System.out.print("Enter Machinery State (Worn/Faulty/Critical): ");
         String machineryState = sc.nextLine();
 
-        // UC3 → calculation
+        // UC4 → validation
 
-        double machineFactor = 0;
+        if (armPrecision < 0.0 || armPrecision > 1.0) {
+            System.out.println("Error: Arm precision must be 0.0-1.0");
+        }
+        else if (workerDensity < 1 || workerDensity > 20) {
+            System.out.println("Error: Worker density must be 1-20");
+        }
+        else if (!machineryState.equals("Worn") &&
+                !machineryState.equals("Faulty") &&
+                !machineryState.equals("Critical")) {
+            System.out.println("Error: Unsupported machinery state");
+        }
+        else {
+            // UC3 logic inside valid block
 
-        if (machineryState.equals("Worn"))
-            machineFactor = 1.3;
-        else if (machineryState.equals("Faulty"))
-            machineFactor = 2.0;
-        else if (machineryState.equals("Critical"))
-            machineFactor = 3.0;
+            double machineFactor = 0;
 
-        double hazardRisk = ((1 - armPrecision) * 15) + (workerDensity * machineFactor);
+            if (machineryState.equals("Worn"))
+                machineFactor = 1.3;
+            else if (machineryState.equals("Faulty"))
+                machineFactor = 2.0;
+            else if (machineryState.equals("Critical"))
+                machineFactor = 3.0;
 
-        System.out.println("Robot Hazard Risk Score: " + hazardRisk);
+            double hazardRisk = ((1 - armPrecision) * 15) + (workerDensity * machineFactor);
+
+            System.out.println("Robot Hazard Risk Score: " + hazardRisk);
+        }
 
         sc.close();
     }
