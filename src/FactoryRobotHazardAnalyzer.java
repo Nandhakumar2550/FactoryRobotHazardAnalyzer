0fa0921b55ce59f2755fd3d9e0c987030eccf5f7
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-04-17 17:58:51 +0530

    [Nandhakumar]/UC2-UserInput

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+// NEW → input
+
 public class FactoryRobotHazardAnalyzer {
 
     public static void main(String[] args) {
-        // main → entry point
 
         System.out.println("Factory Robot Hazard Analyzer");
-        // UC1 → display message
+
+        Scanner sc = new Scanner(System.in);
+        // create scanner
+
+        // UC2 → take inputs
+        System.out.print("Enter Arm Precision (0.0 - 1.0): ");
+        double armPrecision = sc.nextDouble();
+
+        System.out.print("Enter Worker Density (1 - 20): ");
+        int workerDensity = sc.nextInt();
+
+        sc.nextLine();
+        // clear buffer
+
+        System.out.print("Enter Machinery State (Worn/Faulty/Critical): ");
+        String machineryState = sc.nextLine();
+
+        // UC2 → echo inputs
+        System.out.println("\nYou Entered:");
+        System.out.println("Arm Precision: " + armPrecision);
+        System.out.println("Worker Density: " + workerDensity);
+        System.out.println("Machinery State: " + machineryState);
+
+        sc.close();
     }
 }
