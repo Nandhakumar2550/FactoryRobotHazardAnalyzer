f0932fab06fb96b39b6a478dcbc588a1b8f4f932
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-04-17 17:31:21 +0530

    Initial commit

---
+++ src/FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,9 @@
+public class FactoryRobotHazardAnalyzer {
+
+    public static void main(String[] args) {
+        // main → entry point
+
+        System.out.println("Factory Robot Hazard Analyzer");
+        // UC1 → display message
+    }
+}
